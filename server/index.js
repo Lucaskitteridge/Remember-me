1aec17a96afa760da77b7d90888ef44c69807887
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-18 17:13:42 -0400

    still setting up server. Bug in the /posts

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const ENV  = process.env.ENV || "development";
 const cors = require('cors');
+
+const postRoutes = require('./routes/posts');
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
